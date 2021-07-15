9912691ee05aabbf81153fdb28d30f61e3fb5a26
Tony DoVu <tpdovu@gmail.com>
2021-07-15 14:12:17 -0500

    fixed port syntax

--- app.js
+++ app.js
@@ -135,6 +135,6 @@ app.post("/compose", function (req, res) {
 });
 
 
-app.listen(process.env.PORT | 3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started.");
 });
