e078f6c84953007cbf5a84033fc89955a7bd1df1
Tony DoVu <tpdovu@gmail.com>
2021-07-24 00:02:34 -0500

    test commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: "heroku_e0af44fdfb12f8a"
 });
 
 
