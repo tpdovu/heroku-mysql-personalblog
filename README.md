6a2250ac1519dc2ea7bed391fe6eab521fda5f53
Tony DoVu <tpdovu@gmail.com>
2021-09-08 16:03:57 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# personal blog project
+This is a personal blog project.
+express/node back end
+
+Link for fully deployed project on heroku: https://herokyu-mysql-personalblog.herokuapp.com/
--- app.js
+++ app.js
@@ -1,45 +1,43 @@
 //jshint esversion:6
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mysql = require("mysql");
-const date = require('date-and-time');
+const date = require("date-and-time");
 
 var pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
 });
-
 
 console.log(process.env.HOST);
 
-
-
-//#region setup content 
-const homeStartingContent = "This is a page for posting personal blog posts. Click on 'personal blog' to go to /compose and write post. Most recent posts are listed at the top.";
-const aboutContent = "My name is Tony! I created this page with a html/css front end and mysql back end. It is hosted on heroku and uses heroku's cloud based sql, clearDB!";
+//#region setup content
+const homeStartingContent =
+  "This is a page for posting personal blog posts. Click on 'personal blog' to go to /compose and write post. Most recent posts are listed at the top.";
+const aboutContent =
+  "My name is Tony! I created this page with a html/css front end and mysql back end. It is hosted on heroku and uses heroku's cloud based sql, clearDB!";
 const contactContent = "Phone number, email.";
 
 const app = express();
 
+app.set("view engine", "ejs");
 
-
-app.set('view engine', 'ejs');
-
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.static("public"));
 
 //#endregion
 app.get("/", function (req, res) {
-  const sql = 'SELECT * FROM posts';
+  const sql = "SELECT * FROM posts";
   pool.getConnection(function (err, connection) {
     if (err) {
       throw err;
@@ -50,7 +48,7 @@ app.get("/", function (req, res) {
       }
       res.render("home", {
         homeStartingContent: homeStartingContent,
-        posts: retrievedPosts
+        posts: retrievedPosts,
       });
       connection.release();
     });
@@ -69,7 +67,7 @@ app.get("/posts/:postTitle/id/:postID", function (req, res) {
       }
       res.render("post", {
         postTitle: result[0].title,
-        postBody: result[0].body
+        postBody: result[0].body,
       });
 
       connection.release();
@@ -79,13 +77,13 @@ app.get("/posts/:postTitle/id/:postID", function (req, res) {
 
 app.post("/compose", function (req, res) {
   const timeCreated = new Date();
-  const dateString = date.format(timeCreated, 'MMM DD YYYY');
+  const dateString = date.format(timeCreated, "MMM DD YYYY");
   const post = {
     title: req.body.postTitle,
     body: req.body.postBody,
-    date_created: dateString
+    date_created: dateString,
   };
-  const sql = 'INSERT INTO posts SET ?'
+  const sql = "INSERT INTO posts SET ?";
   pool.getConnection(function (err, connection) {
     if (err) {
       throw err;
@@ -96,19 +94,19 @@ app.post("/compose", function (req, res) {
       }
       connection.release();
       res.redirect("/");
-    })
-  })
+    });
+  });
 });
 
 app.get("/about", function (req, res) {
   res.render("about", {
-    aboutContent: aboutContent
+    aboutContent: aboutContent,
   });
 });
 
 app.get("/contact", function (req, res) {
   res.render("contact", {
-    contactContent: contactContent
+    contactContent: contactContent,
   });
 });
 
